b360dd138c8c9e5ec608a2026f67d8df2692d99c
Predrag Stefanovic <predrag.stefanovicps@gmail.com>
2025-07-18 09:56:59 -0700

    main dev setup recipe

---
+++ dev_setup.ps1
@@ -0,0 +1,43 @@
+## This command disables User Account Control to run the script without user interaction, it is enabled at the end of the script.
+## To avoid security concerns you can comment it if you prefer, otherwhise please check the software you install is safe and use this command at your own risk.
+Disable-UAC
+$Boxstarter.AutoLogin=$false
+# Install git and clone repository containing scripts and config files
+# TODO: see how to improve install that by using chezmoi (choco install -y chezmoi)
+choco install -y git --params "/GitOnlyOnPath /NoShellIntegration /WindowsTerminal"
+RefreshEnv
+git clone https://github.com/predragstefanovic/winplay.git "$env:USERPROFILE\winplay"
+# Git configuration
+Remove-Item -Path "$env:USERPROFILE\.gitconfig" -Force
+New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.gitconfig" -Target "$env:USERPROFILE\winplay\config\git\.gitconfig"
+# TODO: configure git signature
+
+# Winget configuration
+Remove-Item -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json" -Force
+New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json" -Target "$env:USERPROFILE\winplay\config\winget\settings.json"
+
+#--- Enable developer mode on the system ---
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
+
+#--- Setting up Windows ---
+. "$env:USERPROFILE\winplay\scripts\FileExplorerSettings.ps1"
+. "$env:USERPROFILE\winplay\scripts\RemoveDefaultApps.ps1"
+. "$env:USERPROFILE\winplay\scripts\Tools.ps1"
+. "$env:USERPROFILE\winplay\scripts\IDEs.ps1"
+
+# TODO: install WSL2 / Ubuntu
+# choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+# choco install -y VirtualMachinePlatform -source windowsfeatures
+# wsl --set-default-version 2
+# choco install wsl2 --params "/Version:2 /Retry:true"
+
+# TODO: Docker
+# winget install -e -h --id suse.RancherDesktop
+
+# // windowsfeatures (Windows Sandbox, .NET Framework)
+# // Taskbar (Set-BoxstarterTaskbarOptions)
+
+#--- reenabling critial items ---
+Enable-UAC
+#Enable-MicrosoftUpdate
+#Install-WindowsUpdate -acceptEula
