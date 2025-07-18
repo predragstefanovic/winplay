4e67e1a7b1b8560f9862d2418e519914a2a3be38
Predrag Stefanovic <predrag.stefanovicps@gmail.com>
2025-07-18 06:13:36 -0700

    Create SystemConfiguration.ps1 - enable developer mode

---
+++ scripts/SystemConfiguration.ps1
@@ -0,0 +1,3 @@
+
+#--- Enable developer mode on the system ---
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
