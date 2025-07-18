4551655bc4e33f3428c9cb9d45b704c0c3bf50c4
Predrag Stefanovic <predrag.stefanovicps@gmail.com>
2025-07-18 09:58:06 -0700

    Add files via upload

---
+++ scripts/RemoveDefaultApps.ps1
@@ -0,0 +1,58 @@
+#--- Uninstall unnecessary applications that come with Windows out of the box ---
+Write-Host "Uninstall some applications that come with Windows out of the box" -ForegroundColor "Yellow"
+
+function removeApp {
+	Param ([string]$appName)
+	Write-Output "Trying to remove $appName"
+	Get-AppxPackage $appName -AllUsers | Remove-AppxPackage
+	Get-AppXProvisionedPackage -Online | Where DisplayName -like $appName | Remove-AppxProvisionedPackage -Online
+}
+
+$applicationList = @(
+	"Microsoft.BingFinance"
+	"Microsoft.3DBuilder"
+	"Microsoft.BingFinance"
+	"Microsoft.BingNews"
+	"Microsoft.BingSports"
+	"Microsoft.BingWeather"
+	"Microsoft.CommsPhone"
+	"Microsoft.Getstarted"
+	"Microsoft.WindowsMaps"
+	"*MarchofEmpires*"
+	"Microsoft.GetHelp"
+	"Microsoft.Messaging"
+	"*Minecraft*"
+	"Microsoft.OneConnect"
+	"Microsoft.WindowsPhone"
+	"Microsoft.WindowsSoundRecorder"
+	"*Solitaire*"
+	"Microsoft.Office.Sway"
+	"Microsoft.XboxApp"
+	"Microsoft.XboxIdentityProvider"
+	"Microsoft.ZuneMusic"
+	"Microsoft.ZuneVideo"
+	"Microsoft.NetworkSpeedTest"
+	"Microsoft.FreshPaint"
+	"Microsoft.Print3D"
+	"*Autodesk*"
+	"*BubbleWitch*"
+    "king.com*"
+    "G5*"
+	"*Facebook*"
+	"*Linkedin*"
+	"*Keeper*"
+	"*Netflix*"
+	"*Twitter*"
+	"*Plex*"
+	"*.Duolingo-LearnLanguagesforFree"
+	"*.EclipseManager"
+	"ActiproSoftwareLLC.562882FEEB491" # Code Writer
+	"*.AdobePhotoshopExpress"
+);
+
+foreach ($app in $applicationList) {
+    removeApp $app
+}
+
+# Disables suggested apps in the start menu
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name SystemPaneSuggestionsEnabled -Type DWord -Value 0
---
+++ scripts/Tools.ps1
@@ -0,0 +1,87 @@
+# ---------------------------------------------- #
+# Browsers  ------------------------------------ #
+# ---------------------------------------------- #
+choco install -y googlechrome
+
+# ---------------------------------------------- #
+# Common tools  --------------------------------- #
+# ---------------------------------------------- #
+choco install -y 7zip
+winget install -e -h --id Microsoft.BingWallpaper
+winget install -e -h --id JohnMacFarlane.Pandoc
+winget install -e -h --id Microsoft.Whiteboard -s msstore
+winget install -e -h --id Microsoft.PowerToys # settings to sync
+# Already  installed by default
+# winget install -e -h --id Microsoft.Teams
+# winget install -e -h --id Microsoft.Office
+winget install -e -h --id Dell.DisplayManager
+
+# ---------------------------------------------- #
+# Dev tools  ----------------------------------- #
+# ---------------------------------------------- #
+winget install -e -h --id AndreasWascher.RepoZ
+# iwr -useb get.scoop.sh | iex
+winget install -e -h --id GitHub.cli
+
+# ---------------------------------------------- #
+# Prompt  -------------------------------------- #
+# ---------------------------------------------- #
+pwsh -Command { Install-Module posh-git -Scope CurrentUser -Force}
+winget install -e -h --id JanDeDobbeleer.OhMyPosh
+
+# ---------------------------------------------- #
+# PowerShell  ---------------------------------- #
+# ---------------------------------------------- #
+winget install -e -h --id Microsoft.PowerShell
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+Remove-Item -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Force
+New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Target "$env:USERPROFILE\winplay\config\powerShell\Microsoft.PowerShell_profile.ps1"
+
+# Uncomment if you want nushell
+# ---------------------------------------------- #
+# NuShell  ---------------------------------- #
+# ---------------------------------------------- #
+#winget install -e -h --id Nushell.Nushell
+# saves an initialization script to ~/.oh-my-posh.nu that will be used in Nushell config file
+#oh-my-posh init nu --config "$env:USERPROFILE\winplay\config\prompt\.oh-my-posh.omp.json"
+#Remove-Item -Path "$env:USERPROFILE\AppData\Roaming\nushell\config.nu" -Force
+#New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Roaming\nushell\config.nu" -Target "$env:USERPROFILE\winplay\config\nu\config.nu"
+# config auto complete dotnet / nuke / ...
+
+# ---------------------------------------------- #
+# Windows Terminal ----------------------------- #
+# ---------------------------------------------- #
+# Windows Terminal (stable + preview) install with Cascadia Code PL font
+winget install -e -h --id Microsoft.WindowsTerminal -s msstore
+winget install -e -h --id Microsoft.WindowsTerminalPreview -s msstore
+choco install -y cascadia-code-nerd-font
+# Windows terminal configuration
+Remove-Item -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force
+New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Target "$env:USERPROFILE\winplay\config\windowsTerminal\settings.json"
+cp "$env:USERPROFILE\winplay\config\windowsTerminal\icons\*" "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\RoamingState\"
+# Windows terminal preview configuration
+Remove-Item -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -Force
+New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -Target "$env:USERPROFILE\winplay\config\windowsTerminal\settings.json"
+cp "$env:USERPROFILE\winplay\config\windowsTerminal\icons\*" "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\RoamingState\"
+
+# ---------------------------------------------- #
+# Azure tools  --------------------------------- #
+# ---------------------------------------------- #
+winget install -e -h --id Microsoft.AzureCLI
+winget install -e -h --id Microsoft.AzureCosmosEmulator
+winget install -e -h --id Microsoft.AzureDataStudio
+winget install -e -h --id Microsoft.azure-iot-explorer
+winget install -e -h --id Microsoft.AzureStorageExplorer
+winget install -e -h --id Pulumi.Pulumi
+winget install -e -h --id Microsoft.AzureFunctionsCoreTools
+# Azurite can be installed through vscode extension or as a global npm package
+# pnpm add -g azurite
+
+
+
+# ---------------------------------------------- #
+# Blog tools  ---------------------------------- #
+# ---------------------------------------------- #
+# wyam
+# netlify
+# statiq
