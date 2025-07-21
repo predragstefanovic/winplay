fe919519fe84853d892f40f38ad57f4d585bf054
pstefanovic <predrag.stefanovic@deliveryhero.com>
2025-07-21 21:42:15 +0200

    clean up, start refactoring shell setup

--- scripts/Tools.ps1
+++ scripts/Tools.ps1
@@ -15,11 +15,9 @@ choco install -y googlechrome
 #region Common Tools
 Write-Host "Installing Common Tools..." -ForegroundColor Yellow
 choco install -y 7zip
-winget install -e -h --id Microsoft.BingWallpaper
 winget install -e -h --id JohnMacFarlane.Pandoc
 winget install -e -h --id Microsoft.Whiteboard -s msstore
 winget install -e -h --id Microsoft.PowerToys # Note: Settings for PowerToys should be synced separately.
-winget install -e -h --id Dell.DisplayManager
 #endregion
 
 #region Development Tools
@@ -29,55 +27,57 @@ winget install -e -h --id GitHub.cli
 choco install -y python
 #endregion
 
-#region PowerShell Configuration
-Write-Host "Configuring PowerShell..." -ForegroundColor Yellow
-winget install -e -h --id Microsoft.PowerShell
-RefreshEnv
-# Create a symbolic link for the PowerShell profile.
-Remove-Item -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Force -ErrorAction SilentlyContinue
-New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\Documents\\PowerShell\Microsoft.PowerShell_profile.ps1" -Target "$env:USERPROFILE\winplay\config\powerShell\Microsoft.PowerShell_profile.ps1"
-#endregion
+# TODO: replace with windows terminal and ohmyzsh setup
 
-#region Prompt Customization
-Write-Host "Configuring Prompt..." -ForegroundColor Yellow
-winget install -e -h --id JanDeDobbeleer.OhMyPosh
-RefreshEnv
-pwsh -Command { Install-Module posh-git -Scope CurrentUser -Force }
-#endregion
+# #region PowerShell Configuration
+# Write-Host "Configuring PowerShell..." -ForegroundColor Yellow
+# winget install -e -h --id Microsoft.PowerShell
+# RefreshEnv
+# # Create a symbolic link for the PowerShell profile.
+# Remove-Item -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Force -ErrorAction SilentlyContinue
+# New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\Documents\\PowerShell\Microsoft.PowerShell_profile.ps1" -Target "$env:USERPROFILE\winplay\config\powerShell\Microsoft.PowerShell_profile.ps1"
+# #endregion
 
-#region Nushell Configuration
-Write-Host "Configuring NuShell..." -ForegroundColor Yellow
-winget install -e -h --id Nushell.Nushell
-RefreshEnv
-# saves an initialization script to ~/.oh-my-posh.nu that will be used in Nushell config file
-oh-my-posh init nu --config "$env:USERPROFILE\winplay\config\prompt\.oh-my-posh.omp.json"
-Remove-Item -Path "$env:USERPROFILE\AppData\Roaming\nushell\config.nu" -Force
-New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Roaming\nushell\config.nu" -Target "$env:USERPROFILE\winplay\config\nu\config.nu"
-#endregion
+# #region Prompt Customization
+# Write-Host "Configuring Prompt..." -ForegroundColor Yellow
+# winget install -e -h --id JanDeDobbeleer.OhMyPosh
+# RefreshEnv
+# pwsh -Command { Install-Module posh-git -Scope CurrentUser -Force }
+# #endregion
 
-#region Windows Terminal Configuration
-Write-Host "Configuring Windows Terminal..." -ForegroundColor Yellow
-# Install Windows Terminal (Stable and Preview) and Cascadia Code Nerd Font.
-winget install -e -h --id Microsoft.WindowsTerminal
-choco install -y cascadia-code-nerd-font
+# #region Nushell Configuration
+# Write-Host "Configuring NuShell..." -ForegroundColor Yellow
+# winget install -e -h --id Nushell.Nushell
+# RefreshEnv
+# # saves an initialization script to ~/.oh-my-posh.nu that will be used in Nushell config file
+# oh-my-posh init nu --config "$env:USERPROFILE\winplay\config\prompt\.oh-my-posh.omp.json"
+# Remove-Item -Path "$env:USERPROFILE\AppData\Roaming\nushell\config.nu" -Force
+# New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Roaming\nushell\config.nu" -Target "$env:USERPROFILE\winplay\config\nu\config.nu"
+# #endregion
 
-# Configure Windows Terminal settings and icons.
-$terminalSettingsPath = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-$terminalIconsPath = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\RoamingState\"
-Remove-Item -Path $terminalSettingsPath -Force -ErrorAction SilentlyContinue
-New-Item -ItemType SymbolicLink -Path $terminalSettingsPath -Target "$env:USERPROFILE\winplay\config\windowsTerminal\settings.json"
-Copy-Item -Path "$env:USERPROFILE\winplay\config\windowsTerminal\icons\*" -Destination $terminalIconsPath -Force
-#endregion
+# #region Windows Terminal Configuration
+# Write-Host "Configuring Windows Terminal..." -ForegroundColor Yellow
+# # Install Windows Terminal (Stable and Preview) and Cascadia Code Nerd Font.
+# winget install -e -h --id Microsoft.WindowsTerminal
+# choco install -y cascadia-code-nerd-font
+
+# # Configure Windows Terminal settings and icons.
+# $terminalSettingsPath = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+# $terminalIconsPath = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\RoamingState\"
+# Remove-Item -Path $terminalSettingsPath -Force -ErrorAction SilentlyContinue
+# New-Item -ItemType SymbolicLink -Path $terminalSettingsPath -Target "$env:USERPROFILE\winplay\config\windowsTerminal\settings.json"
+# Copy-Item -Path "$env:USERPROFILE\winplay\config\windowsTerminal\icons\*" -Destination $terminalIconsPath -Force
+# #endregion
 
 #region Azure Tools
 Write-Host "Installing Azure Tools..." -ForegroundColor Yellow
 winget install -e -h --id Microsoft.AzureCLI
-winget install -e -h --id Microsoft.AzureCosmosEmulator
-winget install -e -h --id Microsoft.AzureDataStudio
-winget install -e -h --id Microsoft.azure-iot-explorer
-winget install -e -h --id Microsoft.AzureStorageExplorer
-winget install -e -h --id Pulumi.Pulumi
-winget install -e -h --id Microsoft.AzureFunctionsCoreTools
+# winget install -e -h --id Microsoft.AzureCosmosEmulator
+# winget install -e -h --id Microsoft.AzureDataStudio
+# winget install -e -h --id Microsoft.azure-iot-explorer
+# winget install -e -h --id Microsoft.AzureStorageExplorer
+# winget install -e -h --id Pulumi.Pulumi
+# winget install -e -h --id Microsoft.AzureFunctionsCoreTools
 #endregion
 
 Write-Host "Tool installation and configuration complete." -ForegroundColor Green
