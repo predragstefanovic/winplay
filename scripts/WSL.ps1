73f6b03ba49c87a790370573a0d9749fb3a42c38
pstefanovic <predrag.stefanovic@deliveryhero.com>
2025-07-22 15:38:55 +0200

    support restart after enabling wsl feature

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -6,10 +6,32 @@
     installs the latest WSL kernel update, sets WSL2 as the default.
 #>
 
+function Get-WindowsFeatureState {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory)]
+        [string]$FeatureName
+    )
+
+    try {
+        $feature = Get-WindowsOptionalFeature -Online -FeatureName $FeatureName -ErrorAction Stop
+        return $feature.State -eq 'Enabled'
+    } catch {
+        Write-Warning "Feature not found: $FeatureName"
+        return $False
+    }
+}
+
 # Enable required system features
 Write-Host "Enabling system features to run WSL..." -ForegroundColor Yellow
+$wslEnabledBefore = Get-WindowsFeatureState "Microsoft-Windows-Subsystem-Linux"
 choco install -y Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'"
+$wslEnabledAfter = Get-WindowsFeatureState "Microsoft-Windows-Subsystem-Linux"
 
+if ($wslEnabledBefore -ne $wslEnabledAfter) {
+    Write-Host "After enabling WSL, restart is necessary ..." -ForegroundColor Yellow
+    Invoke-Reboot
+}
 
 # Download and install the latest WSL kernel update.
 Write-Host "Installing latest WSL kernel update..." -ForegroundColor Yellow
